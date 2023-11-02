946d04527df4e2a07e25b86b5cfe3c15edc07d5e
Subrat Kumar Behera <118833161+subrat1096@users.noreply.github.com>
2023-11-02 23:52:25 +0530

    third commit

--- 01-basic-react/src/App.js
+++ 01-basic-react/src/App.js
@@ -1,5 +1,12 @@
+import Chai from "./chai";
+
 function App() {
-  return <h1>Welcome to Learn React With CodeXP </h1>;
+  return (
+    <>
+      <h1>Welcome to Learn React With CodeXP </h1>
+      <Chai />
+    </>
+  );
 }
 
 export default App;
--- 01-vite-react/src/App.jsx
+++ 01-vite-react/src/App.jsx
@@ -1,5 +1,10 @@
+import Chai from "./code";
+
 function App() {
-  return <h1>Welcome to Learn React with Vite Bundler with CodeXP</h1>;
+  return (
+    // <h1>Welcome to Learn React with Vite Bundler with CodeXP</h1>
+    <Chai />
+  );
 }
 
 export default App;
