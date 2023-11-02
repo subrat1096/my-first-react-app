fa7d38570c80cd4ba5dff8e78a94779a2f229c34
Subrat Kumar Behera <118833161+subrat1096@users.noreply.github.com>
2023-11-02 22:43:50 +0530

    React basic creation method

---
+++ 01-basic-react/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Welcome to Learn React With CodeXP </h1>;
+}
+
+export default App;
---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Welcome to Learn React with Vite Bundler with CodeXP</h1>;
+}
+
+export default App;
